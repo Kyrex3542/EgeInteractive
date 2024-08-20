23dca017cd2cbb468101dc7046687033c5c9a694
İbrahim SARGIN <ibrahimsarginnn@gmail.com>
2024-08-20 20:30:17 +0300

    1

--- Epic Tower Defense Demo/Assets/Scripts/Tower.cs
+++ Epic Tower Defense Demo/Assets/Scripts/Tower.cs
@@ -8,8 +8,16 @@ public class Tower : MonoBehaviour
     [SerializeField] private GameObject towerBase;
     [SerializeField] private GameObject target;
 
+
+    private void LookAt()
+    {
+        if (target != null) return;
+        //float angle=MathF.Atan2(targetPos.y-transform.position.y, targetPos.x-transform.position.x)*Mathf.Rad2Deg;   
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        target=collision.gameObject;
+
 
     }
 }
