6e2ea4edee0578625d1950c055845bfbaeae2e86
Artyy <hasif_alp@hotmail.com>
2024-09-05 17:57:26 +0300

    Multiple Paths & Spawn Locs
    .

--- Epic Tower Defense Demo/Assets/A-Scripts/MapLoader.cs
+++ Epic Tower Defense Demo/Assets/A-Scripts/MapLoader.cs
@@ -8,26 +8,36 @@ public class MapLoader : MonoBehaviour
     [Header("Active Map")]
     public Tilemap activeMap;
     public Tilemap activeMapShadow;
-    public Transform spawnPoint;
+    public Transform[] spawnPoint;
     public Transform targetPoint;
+    [Header("Path Locations & Spawn Point")]
+    public List<PathLocations> PathLocationList = new List<PathLocations>();
+    [System.Serializable]
+    public class PathLocations
+    {
+        public GameObject[] pathsParentList;
+        public Transform[] spawnPoints;
+    }
     [Header("Map List")]
     [SerializeField] private List<Tilemap> tileMaps;
     [SerializeField] private List<Tilemap> tileMapShadows;
-    [SerializeField] private List<GameObject> pathsParentList;
-    [SerializeField]private List<Transform> spawnPoints;
-    [SerializeField]private List<Transform> targetPoints;
+    [SerializeField] private List<Transform> targetPoints;
     [Header("Dont Change")]
     [SerializeField] private GameObject tileMapGrid;
     [SerializeField] private int mapNumber = 0;
-    [SerializeField] private GameObject pathsParent;
+    [SerializeField] private GameObject[] pathsParent;
     private void Awake()
     {
-        mapNumber= PlayerPrefs.GetInt(Player.MAPNUMBERPLAYERPREFS,0);
+        mapNumber = PlayerPrefs.GetInt(Player.MAPNUMBERPLAYERPREFS, 0);
         SetMapVariables();
     }
-    public GameObject GetPathsParent()
+    public GameObject[] GetPathsParent()
     {
         return pathsParent;
+    }
+    public Transform[] GetSpawnLocs()
+    {
+        return spawnPoint;
     }
     private void Update()
     {
@@ -38,19 +48,23 @@ public class MapLoader : MonoBehaviour
     }
     private void SetMapVariables()
     {
+        PathLocations pathLocations = PathLocationList[mapNumber];
         if (mapNumber < tileMaps.Count && mapNumber < tileMapShadows.Count)
         {
             activeMap = tileMaps[mapNumber];
             activeMapShadow = tileMapShadows[mapNumber];
-            pathsParent = pathsParentList[mapNumber];
-            spawnPoint = spawnPoints[mapNumber];
+            pathsParent = pathLocations.pathsParentList;
+            spawnPoint = pathLocations.spawnPoints;
             targetPoint = targetPoints[mapNumber];
             if (activeMap != null && activeMapShadow != null)
             {
                 activeMap = Instantiate(activeMap, Vector3.zero, Quaternion.identity, tileMapGrid.transform);
                 activeMapShadow = Instantiate(activeMapShadow, new Vector3(0, 0, 0.1f), Quaternion.identity, tileMapGrid.transform);
-                pathsParent = Instantiate(pathsParent, new Vector3(0, 0, 0.1f), Quaternion.identity);
-                spawnPoint = Instantiate(spawnPoint,spawnPoint.position,spawnPoint.rotation);
+                for (int i = 0; i < pathLocations.pathsParentList.Length; i++)
+                {
+                    pathsParent[i] = Instantiate(pathsParent[i], new Vector3(0, 0, 0.1f), Quaternion.identity);
+                    Instantiate(spawnPoint[i], spawnPoint[i].position, spawnPoint[i].rotation);
+                }
                 targetPoint = Instantiate(targetPoint, targetPoint.position, targetPoint.rotation);
             }
         }
--- Epic Tower Defense Demo/Assets/A-Scripts/SpawnerScript.cs
+++ Epic Tower Defense Demo/Assets/A-Scripts/SpawnerScript.cs
@@ -9,27 +9,30 @@ public class SpawnerScript : MonoBehaviour
     public Transform targetPoint;
     [SerializeField] private Transform[] paths;
     [SerializeField] private MapLoader mapLoader;
-    private GameObject pathsParent;
+    private GameObject[] pathsParent;
     private void Start()
     {
         pathsParent = mapLoader.GetPathsParent();
-        paths = new Transform[pathsParent.transform.childCount];
-        for (int i = 0; i < pathsParent.transform.childCount; i++)
+        spawnPoint = mapLoader.GetSpawnLocs();
+        for (int i = 0; i < spawnPoint.Length; i++)
         {
-            paths[i] = pathsParent.transform.GetChild(i);
+            spawnPoint[i] = mapLoader.spawnPoint[i];
         }
-        //spawnPoint = mapLoader.spawnPoint;
         targetPoint = mapLoader.targetPoint;
     }
 
     public int Spawner(GameObject EnemyPrefab)
     {
         int NumOfSpawnLoc = spawnPoint.Length;
-
-        for (int i = 0; i < NumOfSpawnLoc;i++)
+        for (int i = 0; i < NumOfSpawnLoc; i++)
         {
             GameObject gameObject = Instantiate(EnemyPrefab, spawnPoint[i].position, Quaternion.identity);
             PathFinder pathFinder = gameObject.GetComponent<PathFinder>();
+            paths = new Transform[pathsParent[i].transform.childCount];
+            for (int j = 0; j < pathsParent[i].transform.childCount; j++)
+            {
+                paths[j] = pathsParent[i].transform.GetChild(j);
+            }
             if (paths.Length > 0)
             {
                 pathFinder.path = new Transform[paths.Length];
