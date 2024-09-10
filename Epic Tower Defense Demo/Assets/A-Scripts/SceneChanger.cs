775db8702a006524f2796e04e02e9e066fe85e55
Ege Interactive <luckycandir@gmail.com>
2024-09-10 20:45:30 +0300

    cdsa

--- Epic Tower Defense Demo/Assets/A-Scripts/SceneChanger.cs
+++ Epic Tower Defense Demo/Assets/A-Scripts/SceneChanger.cs
@@ -12,78 +12,77 @@ public class SceneChanger : MonoBehaviour
     public GameObject Market;
     public GameObject Madalyalar;
     public GameObject Credits;
+    public GameObject EventLevels;
+    public GameObject Levels;
+    public GameObject activescene;
+    public GameObject Events;
     public AudioSource butonsesi;
-
+    private void Awake()
+    {
+        activescene = MainMenu.gameObject;
+        activescene.gameObject.SetActive(true);
+    }
+    public void LevelsMenu()
+    {
+        activescene.SetActive(false);
+        activescene = Levels;
+        activescene.SetActive(true);
+    }
+    public void eventsmenu()
+    {
+        activescene.SetActive(false);
+        activescene = Events;
+        activescene.SetActive(true);
+    }
+    public void eventlevelsmenu()
+    {
+        activescene.SetActive(false);
+        activescene = EventLevels;
+        activescene.SetActive(true);
+    }
     public void Mainmenu()
     {
-        MainMenu.SetActive(true);
-        Play.SetActive(false);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(false);
-        Market.SetActive(false);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+       activescene = MainMenu;
+        activescene.SetActive(true);
     }
     public void Playmenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(true);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(false);
-        Market.SetActive(false);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+        activescene = Play;
+        activescene.SetActive(true);
     }
     public void Envantermenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(false);
-        Envanter.SetActive(true);
-        Ayarlar.SetActive(false);
-        Market.SetActive(false);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+        activescene = Envanter;
+        activescene.SetActive(true);
     }
     public void Ayarlarmenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(false);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(true);
-        Market.SetActive(false);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+        activescene = Ayarlar;
+        activescene.SetActive(true);
     }
     public void Marketmenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(false);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(false);
-        Market.SetActive(true);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+        activescene = Market;
+        activescene.SetActive(true);
     }
     public void Madalyalarmenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(false);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(false);
-        Market.SetActive(false);
-        Madalyalar.SetActive(true);
-        Credits.SetActive(false);
+        activescene.SetActive(false);
+        activescene = Madalyalar;
+        activescene.SetActive(true);
     }
     public void Creditsmenu()
     {
-        MainMenu.SetActive(false);
-        Play.SetActive(false);
-        Envanter.SetActive(false);
-        Ayarlar.SetActive(false);
-        Market.SetActive(false);
-        Madalyalar.SetActive(false);
-        Credits.SetActive(true);
+        activescene.SetActive(false);
+        activescene = Credits;
+        activescene.SetActive(true);
     }
+    
 
     public void LoadLevel(int mapNumber)
     {
