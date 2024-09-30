4501b7338f5260c7808629974b5d6030199920f7
İbrahim SARGIN <ibrahimsarginnn@gmail.com>
2024-09-30 23:07:06 +0300

    deneme
    This reverts commit d216a5913824094dfbfc89465515bc77a669fee1.

--- Epic Tower Defense Demo/Assets/A-Scripts/MapDesignScripts/MapLoader.cs
+++ Epic Tower Defense Demo/Assets/A-Scripts/MapDesignScripts/MapLoader.cs
@@ -37,7 +37,7 @@ public class MapLoader : MonoBehaviour
     [SerializeField] private GameObject tileMapGrid;
     [SerializeField] public int mapNumber = 0;
     [SerializeField] private GameObject[] pathsParent;
-    [SerializeField] private GameObject[] obstacleSpawnPointsParent;
+    [SerializeField] private GameObject obstacleSpawnPointsParent;
 
 
     private void Awake()
@@ -57,14 +57,13 @@ public class MapLoader : MonoBehaviour
     private void SetMapVariables()
     {
         EpisodeSettings episodeSettings = EpisodeSettingList[mapNumber];
-        if (mapNumber < episodeSettings.tileMaps.Count && mapNumber < episodeSettings.tileMapShadows.Count)
-        {
-            activeMap = episodeSettings.tileMaps[mapNumber];
-            activeMapShadow = episodeSettings.tileMapShadows[mapNumber];
+        
+            activeMap = episodeSettings.tileMaps[0];
+            activeMapShadow = episodeSettings.tileMapShadows[0];
             pathsParent = episodeSettings.pathsParentList;
             spawnPoint = episodeSettings.spawnPoints;
-            obstacleSpawnPointsParent = episodeSettings.obstacleSpawnPointsParentList;
-            targetPoint = episodeSettings.targetPoints[mapNumber];
+            obstacleSpawnPointsParent = episodeSettings.obstacleSpawnPointsParentList[0];
+            targetPoint = episodeSettings.targetPoints[0];
             if (activeMap != null && activeMapShadow != null)
             {
                 activeMap = Instantiate(activeMap, Vector3.zero, Quaternion.identity, tileMapGrid.transform);
@@ -74,9 +73,9 @@ public class MapLoader : MonoBehaviour
                     pathsParent[i] = Instantiate(pathsParent[i], new Vector3(0, 0, 0.1f), Quaternion.identity);
                     Instantiate(spawnPoint[i], spawnPoint[i].position, spawnPoint[i].rotation);
                 }
-                for(int i = 0; i < obstacleSpawnPointsParent[mapNumber].transform.childCount; i++)
+                for(int i = 0; i < obstacleSpawnPointsParent.transform.childCount; i++)
                 {
-                    Instantiate(obstacleSpawnPointsParent[mapNumber].transform.GetChild(i), obstacleSpawnPointsParent[mapNumber].transform.GetChild(i).position, Quaternion.identity);
+                    Instantiate(obstacleSpawnPointsParent.transform.GetChild(i), obstacleSpawnPointsParent.transform.GetChild(i).position, Quaternion.identity);
                 }
                 targetPoint = Instantiate(targetPoint, targetPoint.position, targetPoint.rotation);
                 if(targetPoint.TryGetComponent<EndPointScript>(out EndPointScript endPointScript))
@@ -86,8 +85,9 @@ public class MapLoader : MonoBehaviour
                     endPointScript.UImanager = UImanager;
                     endPointScript.SetHealthUI();
                 }
+                else { Debug.LogError("EndPointScript bulunamadı"); }
             }
-        }
+        
     }
 
     public GameObject[] GetPathsParent()
